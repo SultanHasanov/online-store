3ac226c5d1a9052329872be310e470e33ced8670
sultan <sultanhasanov987@gmail.com>
2024-09-12 23:52:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-
 import Home from "./pages/Home";
 import Orders from "./pages/Orders";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
-import InstallPrompt from "./InstallPrompt"; // Компонент для PWA
-import NavBar from "./NavBar"; // Импортируем компонент панели навигации
+import InstallPrompt from "./InstallPrompt";
+import Layout from "./Layout";
 
 const App = () => {
   const [totalPrice, setTotalPrice] = useState(0);
@@ -23,9 +21,8 @@ const App = () => {
 
   return (
     <Router>
-      <div style={{ minHeight: "100vh", paddingBottom: "60px" }}>
-        {/* Контент приложения */}
-        <InstallPrompt /> {/* Компонент для установки PWA */}
+      <InstallPrompt />
+      <Layout totalPrice={totalPrice}>
         <Routes>
           <Route
             path="/"
@@ -41,10 +38,7 @@ const App = () => {
           <Route path="/cart" element={<Cart cartItems={cartItems} />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
-      </div>
-
-      {/* Фиксированная нижняя панель навигации */}
-      <NavBar totalPrice={totalPrice} />
+      </Layout>
     </Router>
   );
 };
---
+++ src/Layout.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  HomeOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import "./App.css";
+
+const Layout = ({ children, totalPrice }) => {
+  return (
+    <div className="app-container">
+      <div className="content">{children}</div>
+
+      <div className="fixed-icons">
+        <NavLink to="/" className="icon-link">
+          <HomeOutlined />
+        </NavLink>
+        <NavLink to="/orders" className="icon-link">
+          <ProfileOutlined />
+        </NavLink>
+         <NavLink to="/cart" className="icon-link">
+          <span className="cart-total">{totalPrice} ₽</span>
+          <ShoppingCartOutlined />
+        </NavLink>
+        <NavLink to="/profile" className="icon-link">
+          <UserOutlined />
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
